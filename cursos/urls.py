d5c566fd965d7d04a973ba7bf11e6e8bcbfff2f8
farevaloc <felipearevalo87@gmail.com>
2024-07-02 21:09:28 -0500

    commit final jornada

--- cursos/urls.py
+++ cursos/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('curso/<int:pk>/', DetalleCursoView.as_view(), name='detalle_curso'),
     path('curso/<int:pk>/inscribir/', InscribirCursoView.as_view(), name='inscribir_curso'),
-    path('curso/crear/', CrearCursoView.as_view(), name='crear_curso'),
+    path('curso/crear/', CrearCursoView.as_view(), name='crear_cursos'),
     path('mis-cursos/', MisCursosView.as_view(), name='mis_cursos'),
 ]
